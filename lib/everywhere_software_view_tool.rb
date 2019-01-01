2c38545ebab89f69ed83da4f3530c1eef0ac81b8
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-12-31 23:11:27 -0800

    Dev tool initial commit

---
+++ lib/everywhere_software_view_tool.rb
@@ -0,0 +1,6 @@
+require "everywhere_software_view_tool/version"
+require "everywhere_software_view_tool/renderer"
+
+module EverywhereSoftwareViewTool
+  # Your code goes here...
+end
---
+++ lib/everywhere_software_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EverywhereSoftwareViewTool
+  VERSION = "0.1.0"
+end
